492a6c01ea3c4b9027c382a146e73e3e5dfa04cb
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2016-08-04 13:00:03 +0300

    Ensure we have a cinder volume type for SMB job

--- devstack_vm/bin/post_stack.sh
+++ devstack_vm/bin/post_stack.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -x
 
+JOB_TYPE=$1
+
 nova quota-class-update --instances 50 --cores 100 --ram $((51200*4)) --floating-ips 50 --security-groups 50 --security-group-rules 100 default
 cinder quota-class-update --snapshots 50 --volumes 50 --gigabytes 2000 default
+
+if [ "$JOB_TYPE" == "smbfs" ]; then
+    # For the other backend types, devstack will create a volume type for us.
+    # Note that having a volume type is now mandatory.
+    cinder type-create SMBFS
+fi
 
 # NAT
 sudo /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
--- jobs/build_devstack.sh
+++ jobs/build_devstack.sh
@@ -36,5 +36,4 @@ update_local_conf
 run_ssh_cmd_with_retry ubuntu@$FLOATING_IP $DEVSTACK_SSH_KEY "source /home/ubuntu/keystonerc && /home/ubuntu/bin/run_devstack.sh $hyperv01" 5
 
 # run post_stack
-run_ssh_cmd_with_retry ubuntu@$FLOATING_IP $DEVSTACK_SSH_KEY "source /home/ubuntu/keystonerc && /home/ubuntu/bin/post_stack.sh" 5
-
+run_ssh_cmd_with_retry ubuntu@$FLOATING_IP $DEVSTACK_SSH_KEY "source /home/ubuntu/keystonerc && /home/ubuntu/bin/post_stack.sh $JOB_TYPE" 5
