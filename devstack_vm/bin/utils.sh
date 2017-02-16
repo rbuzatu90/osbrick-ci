0ca7a1375c5a50fce51683a140a99bfa56470166
Remus Buzatu <r.buzatu90@gmail.com>
2017-02-16 17:06:29 +0200

    Use git_timed
    Signed-off-by: Remus Buzatu <r.buzatu90@gmail.com>

--- devstack_vm/bin/utils.sh
+++ devstack_vm/bin/utils.sh
@@ -237,6 +237,26 @@ rotate_log () {
     fi
 }
 
+function git_timed {
+    local count=0
+    local timeout=0
+
+    if [[ -n "${GIT_TIMEOUT}" ]]; then
+        timeout=${GIT_TIMEOUT}
+    fi
+
+    until timeout -s SIGINT ${timeout} git "$@"; do
+        echo "Command exited with '$?' [git $@] ... retrying"
+        count=$(($count + 1))
+        echo "timeout ${count} for git call: [git $@]"
+        if [ $count -eq 3 ]; then
+            echo $LINENO "Maximum of 3 git retries reached"
+            exit 1
+        fi
+        sleep 5
+    done
+}
+
 function cherry_pick() {
     commit=$1
     set +e
@@ -245,8 +265,9 @@ function cherry_pick() {
     if [ $? -ne 0 ]
     then
         echo "Ignoring failed git cherry-pick $commit"
-        git checkout --force
+        git cherry-pick --abort
     fi
 
     set -e
 }
+
